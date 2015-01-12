37df506f4c2a77084791a7f9da05bf59937e24af
Johnny C. Lam <jlam88@gmail.com>
2015-01-12 18:28:44 -0500

    Add more documentation for release.sh.

--- release.sh
+++ release.sh
@@ -1,6 +1,30 @@
 #!/bin/sh
 #
-# Generate a zippable addon directory from a Git checkout.
+# release.sh generates a zippable addon directory from a Git checkout.
+#
+# release.sh works by creating a new project directory, checking out external
+# repositories within the project directory, then copying files from the Git
+# checkout into the project directory.  The project directory is then zipped to
+# create a distributable addon zipfile.
+#
+# release.sh reads .pkgmeta and supports the following directives:
+#   - externals
+#   - ignore
+#   - manual-changelog
+#   - package-as
+#
+# release.sh supports the following repository substitution keywords when
+# copying the files from the Git checkout into the project directory.
+#   - @project-version@
+#
+# release.sh assumes that annotated tags are named for the version numbers for
+# the project.  It will identify if the HEAD is tagged and use that as the
+# current version number.  It will search back through parent commits for the
+# previous annotated tag that is a release version number and generate a
+# changelog containing the commits since that previous release tag.
+#
+# By default, release.sh creates releases in a "release" subdirectory of the
+# top-level directory of the Git checkout.
 #
 
 # Project name.
