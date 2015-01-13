6a1b4f5ebbdf94a4a074d9176e62c994d46a1b61
Johnny C. Lam <jlam88@gmail.com>
2015-01-12 20:35:37 -0500

    Make release.sh work in fresh Git repositories.
    Be more resilient if there are no tags in the repository, and
    automatically determine the title of the project from the TOC file.
    
    This makes release.sh more reusable in other projects.

--- release.sh
+++ release.sh
@@ -17,6 +17,9 @@
 # copying the files from the Git checkout into the project directory.
 #   - @project-version@
 #
+# release.sh reads the TOC file, if present, to determine the name of the
+# project.
+#
 # release.sh assumes that annotated tags are named for the version numbers for
 # the project.  It will identify if the HEAD is tagged and use that as the
 # current version number.  It will search back through parent commits for the
@@ -26,9 +29,6 @@
 # By default, release.sh creates releases in a "release" subdirectory of the
 # top-level directory of the Git checkout.
 #
-
-# Project name.
-project="Ovale Spell Priority"
 
 # POSIX tools.
 cat=cat
@@ -52,8 +52,12 @@ zip() {
 	$sevenzip a -tzip $archive "$@"
 }
 
-# Set $topdir to top-level directory of the Git checkout.
+# Variables set via options.
+project=
 topdir=
+releasedir=
+
+# Set $topdir to top-level directory of the Git checkout.
 if [ -z "$topdir" ]; then
 	dir=`pwd`
 	if [ -d "$dir/.git" ]; then
@@ -74,12 +78,14 @@ if [ -z "$topdir" ]; then
 		fi
 	fi
 fi
+
 # Set $releasedir to the directory which will contain the generated addon zipfile.
-releasedir="$topdir/release"
+: ${releasedir:="$topdir/release"}
 
 usage() {
-	echo "Usage: release.sh [-eoz] [-r dir]" >&2
+	echo "Usage: release.sh [-eoz] [-n name] [-r releasedir] [-t topdir]" >&2
 	echo "  -e               Skip checkout of external repositories." >&2
+	echo "  -n name          Set the name of the addon." >&2
 	echo "  -o               Keep existing package directory; just overwrite contents." >&2
 	echo "  -r releasedir    Set directory containing the package directory. Defaults to \`\`\$topdir/release''." >&2
 	echo "  -t topdir        Set top-level directory of Git checkout.  Defaults to \`\`$topdir''." >&2
@@ -87,11 +93,14 @@ usage() {
 }
 
 # Process command-line options
-while getopts ":eor:t:z" opt; do
+while getopts ":eon:r:t:z" opt; do
 	case $opt in
 	e)
 		# Skip checkout of external repositories.
 		skip_externals=true
+		;;
+	n)
+		project="$OPTARG"
 		;;
 	o)
 		# Skip deleting any previous package directory.
@@ -138,21 +147,24 @@ $topdir/*)	;;
 	;;
 esac
 
+# Create the release directory.
+$mkdir -p "$releasedir"
+
 # Expand $topdir and $releasedir to their absolute paths for string comparisons later.
 topdir=`cd "$topdir" && pwd`
 releasedir=`cd "$releasedir" && pwd`
 
 # Get the tag for the HEAD.
-tag=`$git describe HEAD --abbrev=0`
+tag=`$git describe HEAD --abbrev=0 2>/dev/null`
 # Find the previous release tag.
-rtag=`$git describe HEAD~1 --abbrev=0`
+rtag=`$git describe HEAD~1 --abbrev=0 2>/dev/null`
 while true; do
 	# A version string must contain only dots and digits and optionally starts with the letter "v".
 	result=`echo "${rtag#v}" | $sed -e "s/[0-9.]*//"`
 	if [ -z "$result" ]; then
 		break
 	fi
-	rtag=`git describe $rtag~1 --abbrev=0`
+	rtag=`$git describe $rtag~1 --abbrev=0 2>/dev/null`
 done
 # If the current and previous tags match, then the HEAD is not tagged.
 if [ "$tag" = "$rtag" ]; then
@@ -160,78 +172,110 @@ if [ "$tag" = "$rtag" ]; then
 else
 	echo "Current tag: $tag"
 fi
-echo "Previous release tag: $rtag"
-
-# Version number.
-version="$tag"
-if [ -z "$version" ]; then
-	version=`$git describe HEAD`
+if [ -z "$rtag" ]; then
+	echo "No previous release tag found."
+else
+	echo "Previous release tag: $rtag"
 fi
 
 # Simple .pkgmeta processor.
 ignore=
-while read line; do
-	case ${line} in
-	package-as:*)
-		phase=${line%%:*}
-		package=${line#*: }
-		pkgdir="$releasedir/$package"
-		if [ -d "$pkgdir" -a -z "$skip_delete_pkgdir" ]; then
-			echo "Removing previous package directory: $pkgdir"
-			$rm -fr "$pkgdir"
-		fi
-		$mkdir -p "$pkgdir"
-		;;
-	externals:*)
-		phase=${line%%:*}
-		;;
-	ignore:*)
-		phase=${line%%:*}
-		;;
-	manual-changelog:*)
-		phase=${line%%:*}
-		changelog=${line#*: }
-		;;
-	filename:*)
-		if [ "$phase" = "manual-changelog" ]; then
+if [ -f "$topdir/.pkgmeta" ]; then
+	while read line; do
+		case $line in
+		package-as:*)
+			phase=${line%%:*}
+			package=${line#*: }
+			pkgdir="$releasedir/$package"
+			if [ -d "$pkgdir" -a -z "$skip_delete_pkgdir" ]; then
+				echo "Removing previous package directory: $pkgdir"
+				$rm -fr "$pkgdir"
+			fi
+			if [ ! -d "$pkgdir" ]; then
+				$mkdir -p "$pkgdir"
+			fi
+			;;
+		externals:*)
+			phase=${line%%:*}
+			;;
+		ignore:*)
+			phase=${line%%:*}
+			;;
+		manual-changelog:*)
+			phase=${line%%:*}
 			changelog=${line#*: }
-		fi
-		;;
-	*git*|*svn*)
-		if [ "$phase" = "externals" -a -z "$skip_externals" ]; then
-			dir=${line%%:*}
-			uri=${line#*: }
-			$mkdir -p "$pkgdir/$dir"
-			case $uri in
-			git:*)
-				echo "Getting checkout for $uri"
-				$git clone $uri "$pkgdir/$dir"
-				;;
-			svn:*)
-				echo "Getting checkout for $uri"
-				$svn checkout $uri "$pkgdir/$dir"
-				;;
-			esac
-		fi
-		;;
-	*"- "*)
-		if [ "$phase" = "ignore" ]; then
-			pattern=${line#*- }
-			if [ -d "../$pattern" ]; then
-				pattern="$pattern/*"
+			;;
+		filename:*)
+			if [ "$phase" = "manual-changelog" ]; then
+				changelog=${line#*: }
 			fi
-			if [ -z "$ignore" ]; then
-				ignore="$pattern"
-			else
-				ignore="$ignore:$pattern"
+			;;
+		*git*|*svn*)
+			if [ "$phase" = "externals" -a -z "$skip_externals" ]; then
+				dir=${line%%:*}
+				uri=${line#*: }
+				$mkdir -p "$pkgdir/$dir"
+				case $uri in
+				git:*)
+					echo "Getting checkout for $uri"
+					$git clone $uri "$pkgdir/$dir"
+					;;
+				svn:*)
+					echo "Getting checkout for $uri"
+					$svn checkout $uri "$pkgdir/$dir"
+					;;
+				esac
 			fi
-		fi
+			;;
+		*"- "*)
+			if [ "$phase" = "ignore" ]; then
+				pattern=${line#*- }
+				if [ -d "../$pattern" ]; then
+					pattern="$pattern/*"
+				fi
+				if [ -z "$ignore" ]; then
+					ignore="$pattern"
+				else
+					ignore="$ignore:$pattern"
+				fi
+			fi
+			;;
+		esac
+	done < "$topdir/.pkgmeta"
+	$find "$pkgdir" -name .git -print -o -name .svn -print | while read dir; do
+		$rm -fr "$dir"
+	done
+fi
+
+# Set $version to the version number of HEAD.  May be empty if there are no commits.
+version="$tag"
+if [ -z "$version" ]; then
+	version=`$git describe HEAD 2>/dev/null`
+	if [ -z "$version" ]; then
+		version=`$git rev-parse --short HEAD 2>/dev/null`
+	fi
+fi
+
+# Set $package to the basename of the Git checkout directory if not already set.
+if [ -z "$package" ]; then
+	# Use the basename of the Git checkout directory as the package name.
+	case $topdir in
+	/*/*)
+		package=${topdir##/*/}
+		;;
+	/*)
+		package=${topdir##/}
 		;;
 	esac
-done < "$topdir/.pkgmeta"
-$find "$pkgdir" -name .git -print -o -name .svn -print | while read dir; do
-	$rm -fr "$dir"
-done
+fi
+
+# Set $pkgdir to the path of the package directory inside $releasedir.
+if [ -z "$pkgdir" ]; then
+	pkgdir="$releasedir/$package"
+	if [ ! -d "$pkgdir" ]; then
+		$mkdir -p "$pkgdir"
+	fi
+fi
 
 # Copy files from working directory into the package directory.
 # Prune away any files in the .git and release directories.
@@ -300,23 +344,58 @@ $find "$topdir" -name .git -prune -o -name "${releasedir#$topdir/}" -prune -o -p
 	fi
 done
 
-# Create changelog of commits since the previous release tag.
-if [ -z "$changelog" ]; then
-	changelog="CHANGELOG.txt"
+# Find the name of the project if unset.
+if [ -z "$project" ]; then
+	# Parse the TOC file if it exists for the title of the project.
+	if [ -f "$topdir/$package.toc" ]; then
+		while read line; do
+			case $line in
+			"## Title: "*)
+				project=${line#"## Title: "}
+				;;
+			esac
+		done < "$topdir/$package.toc"
+	fi
 fi
-echo "Generating changelog of commits since $rtag into $changelog."
-$cat > "$pkgdir/$changelog" << EOF
-$project $version
+# Default to the name of the package directory.
+: ${project:="$package"}
 
-Changes from version $rtag:
+# Create changelog of commits since the previous release tag.
+if [ -n "$version" ]; then
+	if [ -z "$changelog" ]; then
+		changelog="CHANGELOG.txt"
+	fi
+	if [ -n "$rtag" ]; then
+		echo "Generating changelog of commits since $rtag into $changelog."
+		change_string="Changes from version $rtag:"
+		git_commit_range="$rtag..HEAD"
+	else
+		echo "Generating changelog of commits into $changelog."
+		change_string="All changes:"
+		git_commit_range=
+	fi
+	if [ -n "$version" ]; then
+		project_string="$project $version"
+	else
+		project_string="$project (unreleased)"
+	fi
+	$cat > "$pkgdir/$changelog" << EOF
+$project_string
+
+$change_string
 
 EOF
-$git log $rtag..HEAD --pretty=format:"- %B" >> "$pkgdir/$changelog"
-$sed -i "s/$/\r/" "$pkgdir/$changelog"
+	$git log $git_commit_range --pretty=format:"- %B" >> "$pkgdir/$changelog"
+	$sed -i "s/$/\r/" "$pkgdir/$changelog"
+fi
 
 # Creating the final zipfile for the addon.
 if [ -z "$skip_zipfile" ]; then
-	archive="$releasedir/$package-$version.zip"
+	if [ -n "$version" ]; then
+		archive="$releasedir/$package-$version.zip"
+	else
+		archive="$releasedir/$package-unreleased.zip"
+	fi
 	if [ -f "$archive" ]; then
 		echo "Removing previous archive: $archive"
 		$rm -f "$archive"
