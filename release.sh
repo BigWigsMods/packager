9c6cb234ed632944fbf1b982f37f6f8da08bc7b2
Kyle Buller <bullerk@gmail.com>
2019-08-31 11:14:48 -0500

    Only complain about localization if a replacement keyword is found

--- release.sh
+++ release.sh
@@ -877,7 +877,7 @@ set_localization_url() {
 	if [ -n "$slug" ] && [ -n "$cf_token" ] && [ -n "$project_site" ]; then
 		localization_url="${project_site}/api/projects/$slug/localization/export"
 	fi
-	if [ -z "$localization_url" ]; then
+	if [ -z "$localization_url" ] && grep -rq --include="*.lua" "@localization"; then
 		echo "Skipping localization! Missing CurseForge API token and/or project id is invalid."
 		echo
 	fi
