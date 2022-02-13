dc534d34e7301d71bc666a327681161ee7f42bd0
Moorea Tv <moorea@ymail.com>
2022-02-12 18:53:41 -0800

    update all found toc with current interface version

---
+++ update-versions.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+# LPGL v3 (c) 2022 MooreaTv <moorea@ymail.com>
+
+# shellcheck source=interfaces.txt
+source "$(dirname "${BASH_SOURCE[0]}")/interfaces.txt"
+
+variants="BCC Classic Mainline"
+
+for v in $variants; do
+    echo "Toc for $v is ${!v}"
+done
+
+for v in $variants; do
+    find . -type f -name "*-$v.toc" | xargs sed -i -E -e "s/## *Interface:.*/## Interface: ${!v}/i"
+done
