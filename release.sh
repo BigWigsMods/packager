6137663de369cd805ddac162c7d72b6fc9c6862d
Adrian L Lange <p3lim@users.noreply.github.com>
2022-10-24 22:56:42 +0000

    Use new github action output format

--- release.sh
+++ release.sh
@@ -2469,7 +2469,7 @@ if [ -z "$skip_zipfile" ]; then
 	fi
 
 	if [ -n "$GITHUB_ACTIONS" ]; then
-		echo "::set-output name=archive_path::${archive}"
+		echo "ARCHIVE_PATH=${archive}" >> $GITHUB_OUTPUT
 	fi
 
 	start_group "Creating archive: $archive_name ($archive_label)" "archive"
